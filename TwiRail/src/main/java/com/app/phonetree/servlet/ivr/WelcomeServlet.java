a076430c34876d6ef3f3cda6099e7833b272fb69
Osmel <Osmel@192.168.1.12>
2018-06-09 13:47:14 -0400

    new commit

--- TwiRail/src/main/java/com/app/phonetree/servlet/ivr/WelcomeServlet.java
+++ TwiRail/src/main/java/com/app/phonetree/servlet/ivr/WelcomeServlet.java
@@ -1,5 +1,10 @@
 package com.app.phonetree.servlet.ivr;
 
+import com.twilio.twiml.voice.Gather;
+import com.twilio.twiml.voice.Play; //might not use
+import com.twilio.twiml.TwiMLException;
+import com.twilio.twiml.VoiceResponse;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,29 +16,21 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class WelcomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WelcomeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+   
+	protected void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+		VoiceResponse response = new VoiceResponse.Builder()
+                .gather(new Gather.Builder()
+                        .action("/menu/show")
+                        .numDigits(1)
+                        .build())
+                .build();
+		
+		servletResponse.setContentType("text/xml");
+        try {
+            servletResponse.getWriter().write(response.toXml());
+        } catch (TwiMLException e) {
+            throw new RuntimeException(e);
+        }
 	}
 
 }
