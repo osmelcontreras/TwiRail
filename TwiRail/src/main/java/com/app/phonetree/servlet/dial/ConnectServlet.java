fd20c28fb81b98c055931dcdc6e2224deb68a477
Osmel <Osmel@25.211.9.18>
2018-06-11 18:10:35 -0400

    dial method, and dopost for numbers

--- TwiRail/src/main/java/com/app/phonetree/servlet/dial/ConnectServlet.java
+++ TwiRail/src/main/java/com/app/phonetree/servlet/dial/ConnectServlet.java
@@ -1,10 +1,20 @@
 package com.app.phonetree.servlet.dial;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.twilio.twiml.VoiceResponse;
+import com.app.phonetree.servlet.common.Redirect;
+import com.twilio.twiml.voice.Dial;
+import com.twilio.twiml.voice.Number;
+import com.twilio.twiml.TwiMLException;
+import com.twilio.twiml.VoiceResponse;
 
 /**
  * Servlet implementation class ConnectServlet
@@ -19,21 +29,31 @@ public class ConnectServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+        
+		String selectedOption = servletRequest.getParameter("Digits");
+        Map<String, String> optionPhones = new HashMap<>();
+        optionPhones.put("2", "+12024173378");
+        optionPhones.put("3", "+12027336386");
+        optionPhones.put("4", "+12027336637");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+        VoiceResponse twiMLResponse = optionPhones.containsKey(selectedOption)
+                ? dial(optionPhones.get(selectedOption))
+                : Redirect.toMainMenu();
+
+        servletResponse.setContentType("text/xml");
+        try {
+            servletResponse.getWriter().write(twiMLResponse.toXml());
+        } catch (TwiMLException e) {
+            throw new RuntimeException(e);
+        }
 	}
+	private VoiceResponse dial(String phoneNumber) {
+        Number number = new Number.Builder(phoneNumber).build();
+        return new VoiceResponse.Builder()
+                .dial(new Dial.Builder().number(number).build())
+                .build();
+    }
 
 }
