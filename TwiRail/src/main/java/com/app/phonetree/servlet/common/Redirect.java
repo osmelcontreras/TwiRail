d5bfe2a5d158ace3ecc5540af9a89e2067459b77
Osmel <Osmel@25.211.9.18>
2018-06-11 17:59:07 -0400

    commit

--- TwiRail/src/main/java/com/app/phonetree/servlet/common/Redirect.java
+++ TwiRail/src/main/java/com/app/phonetree/servlet/common/Redirect.java
@@ -1,39 +1,26 @@
 package com.app.phonetree.servlet.common;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Say;
 
-/**
- * Servlet implementation class Redirect
- */
-public class Redirect extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Redirect() {
-        super();
-        // TODO Auto-generated constructor stub
+public final class Redirect {
+
+    private Redirect() {
+        // To prevent instantiation.
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    public static VoiceResponse toMainMenu() {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+  
+        VoiceResponse response = new VoiceResponse.Builder()
+                .say(new Say.Builder("Returning to the main menu")
+                        .voice(Say.Voice.ALICE)
+                        .language(Say.Language.EN_GB)
+                        .build())
+                .redirect(new com.twilio.twiml.voice.Redirect.Builder("/irv/welcome").build())
+                .build();
 
+        return response;
+    }
 }
+
